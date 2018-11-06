aeeba9b2ce6811557793921929142bb15fd2d7cb
Isabella Defilippis <isastdef@gmail.com>
2018-11-06 13:40:22 -0400

    atributos: Bugs fixed

--- KyuCompilerF/SemanticAnalyzer.cs
+++ KyuCompilerF/SemanticAnalyzer.cs
@@ -15,29 +15,24 @@ namespace KyuCompilerF
         public void Evaluar(Nodo nodo)
         {
             exceptions = new List<Exception>();
-            Evaluar(nodo, 1);
+            EvaluarConLineas(nodo);
             if (exceptions.Count > 0)
             {
                 throw new AggregateException(exceptions);
             }
         }
 
-        private void Evaluar(Nodo nodo, int linea)
+        private void EvaluarConLineas(Nodo nodo)
         {
             if (!nodo.FueEvaluado)
             {
-                nodo.Linea = linea;
                 Dictionary<string, List<Simbolo>> simbolosHijos = new Dictionary<string, List<Simbolo>>();
                 simbolosHijos.Add(nodo.Contenido, new List<Simbolo>() { nodo.Symbol });
                 if (nodo.Hijos != null)
                 {
                     foreach (Nodo hijo in nodo.Hijos)
                     {
-                        if (hijo.Contenido == "\n")
-                        {
-                            linea++;
-                        }
-                        Evaluar(hijo, linea);
+                        EvaluarConLineas(hijo);
                         if (!simbolosHijos.ContainsKey(hijo.Contenido))
                         {
                             simbolosHijos.Add(hijo.Contenido, new List<Simbolo>() { hijo.Symbol });
@@ -46,6 +41,11 @@ namespace KyuCompilerF
                         {
                             simbolosHijos[hijo.Contenido].Add(hijo.Symbol);
                         }
+                    }
+                    if (nodo.Hijos.Count > 0)
+                    {
+                        //tomar el menor numero de linea de los hijos
+                        nodo.Linea = nodo.Hijos[0].Linea;
                     }
                     try
                     {
